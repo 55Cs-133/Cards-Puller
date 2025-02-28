09fb98d71247d837becc2b8f7f1d10a6666e5a38
Camille Boulon <hacoeurman@outlook.com>
2025-02-28 18:16:00 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+import { cardsCatalog } from './booster/engine'
+
+console.log(cardsCatalog);
+
+function App() {
+
+
+
+
+
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
---
+++ src/booster/engine.ts
@@ -0,0 +1,4 @@
+import data from '../cartes/cartes.json'
+
+// Tableau qui me permet de vérifier si un nom de Pokémon apparaît bien une seule fois.
+export const cardsCatalog = Object.values(data);
